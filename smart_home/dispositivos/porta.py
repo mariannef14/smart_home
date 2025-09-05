16e8ff32a2be1a87faeeaa1c8dc612020e6974f9
Marianne Félix <mariannefelixs11@gmail.com>
2025-09-05 17:06:19 -0300

    feat: add dispositivo porta

---
+++ smart_home/dispositivos/porta.py
@@ -0,0 +1,79 @@
+from enum import Enum
+from transitions import Machine
+from transitions.core import MachineError
+
+
+class StatesPorta(Enum):
+
+    TRANCADA = 0
+    DESTRANCADA = 1
+    ABERTA = 2
+
+
+    def __str__(self) -> str:
+        return self.name
+
+
+transitions = [
+
+    {
+       "trigger": "destrancar",
+       "source": StatesPorta.TRANCADA,
+       "dest": StatesPorta.DESTRANCADA
+    },
+
+    {
+        "trigger": "trancar",
+        "source": StatesPorta.DESTRANCADA,
+        "dest": StatesPorta.TRANCADA,        
+    },
+
+    {
+        "trigger": "abrir",
+        "source": StatesPorta.DESTRANCADA,
+        "dest": StatesPorta.ABERTA
+    },
+
+    {
+        "trigger": "fechar",
+        "source": StatesPorta.ABERTA,
+        "dest": StatesPorta.DESTRANCADA
+    }
+
+]
+
+class Porta:
+
+    def __init__(self):
+        self.machine = Machine(model = self, states = StatesPorta, transitions = transitions, initial = StatesPorta.TRANCADA, on_exception = 'state_error', send_event = True)
+        self.tentativas_invalidas = 0
+    
+
+    def state_error(self, event):
+
+        self.tentativas_invalidas += 1
+
+        print(event.error)
+
+        #TODO: lançar o raise quando estiver com o menu com o tratamento do try
+        # raise MachineError(event)
+        
+
+if __name__ == '__main__':
+
+    porta = Porta()
+    porta.destrancar()
+    print("Status porta", porta.state)
+    porta.abrir()
+    print("Status porta", porta.state)
+    porta.fechar()
+    print("Status porta", porta.state)
+    porta.trancar()
+    print("Status porta", porta.state)
+    porta.destrancar()
+    print("Status porta", porta.state)
+    porta.abrir()
+    print("Status porta", porta.state)
+    porta.trancar()
+    print("Status porta", porta.state)
+    print(porta.tentativas_invalidas)
