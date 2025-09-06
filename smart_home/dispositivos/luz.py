519072012f9b233aed6fef49ec2e35b656636df0
Marianne Félix <mariannefelixs11@gmail.com>
2025-09-05 23:17:08 -0300

    feat: add method change color and brightness

--- smart_home/dispositivos/luz.py
+++ smart_home/dispositivos/luz.py
@@ -1,21 +1,26 @@
 from transitions import Machine
 from enum import Enum
+from dataclasses import dataclass, field
 
 
 class BrilhoValidator:
 
     def __get__(self, instance, owner):
-        return getattr(instance, "brilho")
+        return getattr(instance, self.private_name)
     
 
     def __set__(self, instance, value):
         
         if value >= 0 and value <= 100:
-           setattr(instance, "brilho", value)
+           setattr(instance, self.private_name, value)
         
         else:
             raise ValueError("Valor do brilho precisa estar entre 0 e 100")
-        
+    
+
+    def __set_name__(self, owner, name):
+        self.private_name = "_" + name
+
 
 class CorEnum(Enum):
 
@@ -27,16 +32,20 @@ class CorEnum(Enum):
 class CorValidator:
 
     def __get__(self, instance, owner):
-        return getattr(instance, "cor")
+        return getattr(instance, self.private_name)
     
 
     def __set__(self, instance, value):
 
         if value.upper() in [cor.name for cor in CorEnum]:
-            setattr(instance, "cor", value)
+            setattr(instance, self.private_name, value)
 
         else:
-            raise ValueError("Cor não disponível")
+            raise ValueError(f"A cor {value} não existe")
+    
+
+    def __set_name__(self, owner, name):
+        self.private_name = "_" + name
 
 
 transitions = [
@@ -57,14 +66,14 @@ transitions = [
         "trigger": "definir_brilho",
         "source": "On",
         "dest": "On",
-        "after": "definir_brilho_luz"
+        "after": "mudar_brilho_luz"
     },
 
     {
         "trigger": "definir_cor",
-        "source": "on",
-        "dest": "on",
-        "after": "definir_cor_luz"
+        "source": "On",
+        "dest": "On",
+        "after": "mudar_cor_luz"
     }
 
 ]
@@ -76,30 +85,41 @@ class Luz:
         self.machine = Machine(model = self, states = ["Off", "On"], transitions = transitions, initial = "Off")
         self.brilho = BrilhoValidator()
         self.cor = CorValidator()
+    
 
-    #TODO: verificar se a luz está ligada
-    def definir_brilho_luz(self, value):
+    def mudar_brilho_luz(self):
+        
+        value = int(input("Digite o valor do brilho: "))
+
+        if value < 0 or value > 100:
+            raise ValueError("Valor do brilho precisa estar entre 0 e 100")
+
         self.brilho = value
-    
-    
-    def definir_cor_luz(self, value):
-        self.cor = value
 
+    
+    def mudar_cor_luz(self):
 
+        value = input("Digite a cor: ").upper().strip()
+
+        if value in [cor.name for cor in CorEnum]:
+            self.cor = value
+        
+        else:
+            raise ValueError(f"A cor de luz {value} não existe")
+
+       
 
 if __name__ == '__main__':
 
     luz = Luz()
     luz.ligar()
-    print("Status luz", luz.state)
-    luz.definir_brilho(20)
-    print("Status luz", luz.state)
-    luz.definir_cor_luz("fria")
-    print("Status luz", luz.state)
-    print("Brilho: ", luz.brilho)
-    print("Cor", luz.cor)
-    luz.definir_brilho(101)
+    print("Status luz: ", luz.state)
+
+    luz.definir_brilho()
+    print(luz.brilho)
+
+    luz.definir_cor()
+    print(luz.cor)
+
     luz.desligar()
-    print("Status luz", luz.state)
-
-
+    print("Status luz: ", luz.state)
