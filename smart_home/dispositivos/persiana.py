bf16a25d75ac42c46e35b79c6d11746801f3bd4a
Marianne Félix <mariannefelixs11@gmail.com>
2025-09-07 20:44:24 -0300

    feat: add dispositivo persiana

---
+++ smart_home/dispositivos/persiana.py
@@ -0,0 +1,64 @@
+from transitions import Machine
+from dataclasses import dataclass, field
+
+from smart_home.core.validators import PorcentagemValidator
+
+
+transitions = [
+
+    {
+        "trigger": "abrir",
+        "source": "Closed",
+        "dest": "Open"
+
+    },
+
+    {
+        "trigger": "definir_porcentagem_abertura",
+        "source": "Open",
+        "dest": "Open",
+        "after": "porcentagem_abertura"
+
+    },
+
+    {
+        "trigger": "fechar",
+        "source": "Open",
+        "dest": "Closed"
+    }
+    
+]
+
+
+@dataclass
+class Persiana:
+
+    porcentagem_aberta:int = field(init = False, default = PorcentagemValidator())
+
+
+    def __post_init__(self):
+        self.machine = Machine(model = self, states = ["Open", "Closed"], transitions = transitions, initial = "Closed")
+
+
+    def on_enter_Closed(self):
+        self.porcentagem_aberta = 0
+    
+
+    def porcentagem_abertura(self):
+        
+        porcentagem = int(input("Deseja abrir quantos porcentos da persiana?"))
+
+        self.porcentagem_aberta = porcentagem
+
+
+if __name__ == "__main__":
+
+    persiana = Persiana()
+    persiana.abrir()
+    print(persiana.state)
+    persiana.definir_porcentagem_abertura()
+    print(persiana.state)
+    print(f"Persiana com {persiana.porcentagem_aberta}% aberta")
+    persiana.fechar()
+    print(persiana.state)
+    print(persiana.porcentagem_aberta)
