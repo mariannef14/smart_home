5f43378915765a9e73699e389e07007013d621f1
Marianne Félix <mariannefelixs11@gmail.com>
2025-09-07 20:43:08 -0300

    refactor: change class to dataclass

--- smart_home/dispositivos/luz.py
+++ smart_home/dispositivos/luz.py
@@ -1,51 +1,8 @@
 from transitions import Machine
-from enum import Enum
 from dataclasses import dataclass, field
 
-
-class BrilhoValidator:
-
-    def __get__(self, instance, owner):
-        return getattr(instance, self.private_name)
-    
-
-    def __set__(self, instance, value):
-        
-        if value >= 0 and value <= 100:
-           setattr(instance, self.private_name, value)
-        
-        else:
-            raise ValueError("Valor do brilho precisa estar entre 0 e 100")
-    
-
-    def __set_name__(self, owner, name):
-        self.private_name = "_" + name
-
-
-class CorEnum(Enum):
-
-    NEUTRA = 0,
-    FRIA = 1,
-    QUENTE = 2
-
-
-class CorValidator:
-
-    def __get__(self, instance, owner):
-        return getattr(instance, self.private_name)
-    
-
-    def __set__(self, instance, value):
-
-        if value.upper() in [cor.name for cor in CorEnum]:
-            setattr(instance, self.private_name, value)
-
-        else:
-            raise ValueError(f"A cor {value} não existe")
-    
-
-    def __set_name__(self, owner, name):
-        self.private_name = "_" + name
+from smart_home.core.validators import BrilhoValidator, CorValidator
+from smart_home.core.dispositivos import CorEnum
 
 
 transitions = [
@@ -79,20 +36,20 @@ transitions = [
 ]
 
 
+@dataclass
 class Luz:
 
-    def __init__(self):
+    brilho:int = field(init = False, default = BrilhoValidator())
+    cor:CorEnum = field(init = False, default = CorValidator())
+
+
+    def __post_init__(self):
         self.machine = Machine(model = self, states = ["Off", "On"], transitions = transitions, initial = "Off")
-        self.brilho = BrilhoValidator()
-        self.cor = CorValidator()
-    
+     
 
     def mudar_brilho_luz(self):
         
         value = int(input("Digite o valor do brilho: "))
-
-        if value < 0 or value > 100:
-            raise ValueError("Valor do brilho precisa estar entre 0 e 100")
 
         self.brilho = value
 
@@ -101,13 +58,9 @@ class Luz:
 
         value = input("Digite a cor: ").upper().strip()
 
-        if value in [cor.name for cor in CorEnum]:
-            self.cor = value
-        
-        else:
-            raise ValueError(f"A cor de luz {value} não existe")
-
+        self.cor = value
        
+
 
 if __name__ == '__main__':
 
