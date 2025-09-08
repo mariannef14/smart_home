f935e21895dff948154562773a7e3f908b6413d2
Marianne Félix <mariannefelixs11@gmail.com>
2025-09-08 15:36:35 -0300

    feat: add options 1,2,3,8,9 e 10

--- smart_home/core/cli.py
+++ smart_home/core/cli.py
@@ -1,3 +1,8 @@
+from smart_home.core.hub import Hub
+
+hub = Hub()
+
+
 while True:
     print("=== SMART HOME HUB ===")
     print("1. Listar dispositivos")
@@ -10,7 +15,31 @@ while True:
     print("8. Adicionar dispositivo")
     print("9. Remover dispositivo")
     print("10. Sair")
-    opcao = int(input("Escolha uma opcao:"))
+    opcao = int(input("Escolha uma opcao: "))
 
-    if opcao == 10:
-        break
+
+    try:
+
+        if opcao == 1:
+            hub.listar_dispositivos()
+        
+        elif opcao == 2:
+            id_dispositivo = input("Digite o id do dispositivo que deseja ver mais detalhes:")
+            print(hub.mostrar_dispositivo())
+        
+        #TODO: FALTA FINALIZAR
+        elif opcao == 3:
+            hub.executar_comando()
+
+        elif opcao == 8:
+            hub.adicionar_dispositivo()
+
+        #TODO: FALTA INICIAR
+        elif opcao == 9:
+            hub.remover_dispositivo()
+
+        elif opcao == 10:
+            break
+    
+    except Exception as error:
+        print(error)
