bfb2727ba929967010807bc95e6f74e1ae04f76b
Marianne Félix <mariannefelixs11@gmail.com>
2025-09-07 22:35:32 -0300

    feat: add cli smart hub

---
+++ smart_home/core/cli.py
@@ -0,0 +1,16 @@
+while True:
+    print("=== SMART HOME HUB ===")
+    print("1. Listar dispositivos")
+    print("2. Mostrar dispositivo")
+    print("3. Executar comando em dispositivo")
+    print("4. Alterar atributo de dispositivo")
+    print("5. Executar rotina")
+    print("6. Gerar relatorio")
+    print("7. Salvar configuracao")
+    print("8. Adicionar dispositivo")
+    print("9. Remover dispositivo")
+    print("10. Sair")
+    opcao = int(input("Escolha uma opcao:"))
+
+    if opcao == 10:
+        break
