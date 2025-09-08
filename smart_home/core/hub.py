9d9e7e603368218f374081e4670f69eb457e52d1
Marianne Félix <mariannefelixs11@gmail.com>
2025-09-08 15:38:26 -0300

    feat: add adicionar dispositivo Porta e Luz

--- smart_home/core/hub.py
+++ smart_home/core/hub.py
@@ -1,28 +1,98 @@
 from transitions import Machine
+from dataclasses import dataclass, field
+from typing import List
 
-from smart_home.dispositivos import Porta, Luz, Tomada, Persiana
+from smart_home.core.dispositivos import TiposDispostivos
+from smart_home.dispositivos.porta import Porta
 
 
+@dataclass
 class Hub:
 
-    def __init__(self, porta = None, luz = None, tomada = None, camera = None, irrigador = None, persiana = None):
-        self.porta = porta
-        self.luz = luz
-        self.tomada = tomada
-        self.camera = camera
-        self.irrigador = irrigador
-        self.persiana = persiana
-        self.FMS = []
+    dispositivos:List = field(default_factory = list)
 
-        for dispositivo in self.__dict__():
-            if dispositivo is not None:
-                self.FMS.append(dispositivo)
-        #self.FMS = [self.porta, self.luz, self.tomada, self.camera, self.irrigador, self.persiana]
+    
+    def adicionar_dispositivo(self):
+        print("Tipos suportados:", TiposDispostivos.all_dispositives())
+        tipo_dispositivo = input("Tipo do dispositivo: ").title().strip()
+        id_dispositivo = input("Id (sem espacos): ")
+        nome_dispositivo = input("Nome: ")
+
+
+        if tipo_dispositivo == "Porta":
+            porta = Porta("porta_sala", "porta da sala")
+
+            self._adicionar_porta(id_dispositivo, nome_dispositivo)
+            # self._adicionar_porta(porta.id, porta.nome)
+        
+        elif tipo_dispositivo == "Luz":
+            self_adicionar_luz(id_dispositivo, nome_dispositivo)
     
 
-    @property
-    def dispositivos_da_casa(self):
-        return self.FMS
+    def _adicionar_porta(self, id:str, nome:str):
+
+        porta = Porta(id, nome)
+        porta_dict = {
+            "id": porta.id,
+            "nome": porta.nome,
+            "tipo": porta.tipo,
+            "estado": porta.state.name
+        }
+
+        self.dispositivos.append(porta_dict)
+
+        # lista_dispositivos = self.dispositivos.get('porta', [])
+
+        # lista_dispositivos.append(porta)
+
+        # self.dispositivos['porta'] = lista_dispositivos
+    
+
+    def mostrar_dispositivo(self, id_dispositivo):
+
+        # id_dispositivo = input("Digite o id do dispositivo que deseja ver mais detalhes:")
+
+        
+        for dispositivo in self.dispositivos:
+            
+            if dispositivo.get("id") == id_dispositivo.lower().strip():
+                # print("ID | NOME | TIPO | ESTADO")
+                # return f"{dispositivo.get('id')} | {dispositivo.get('nome')} | {dispositivo.get('tipo')} | {dispositivo.get('estado')}"
+                return f"ID: {dispositivo.get('id')} | Nome: {dispositivo.get('nome')} | Tipo: {dispositivo.get('tipo')} | Estado: {dispositivo.get('estado')}"
+                
+        #TODO: LANÇAR EXCEÇÃO PERSONALIZADA PARA DISPOSITIVO NÃO ENCONTRADO
+        print("Não encontrou")
+
+
+    #TODO: PRINT COM CADA DISPOSITIVO IMPRIMINDO COM O REP DE CADA DISPOSITIVO
+    def listar_dispositivos(self):
+        
+        # print("ID | TIPO | ESTADO")
+        for dispositivo in self.dispositivos:
+            print(f"ID: {dispositivo.get('id')} | Tipo: {dispositivo.get('tipo')} | Estado: {dispositivo.get('estado')}")
+            # print(f"{dispositivo.get('id')} | {dispositivo.get('tipo')} | {dispositivo.get('estado')}")
+
+
+    def remover_dispositivo(self):
+
+        id_dispositivo = input("Id do dispositivo: ")
+
+        for dispositivo in self.dispositivos:
+            
+            if dispositivo.get("id") == id_dispositivo.lower().strip():
+                print(self.dispositivos[dispositivo]) #ver se printa o dispositivo correto
+                # self.dispositivos.remove(dispositivo)
+                # del[dispositivo]
+                dispositivo.remove(id_dispositivo)
+
+
+    def executar_comando(self):
+
+        id_dispositivo = input("Id do dispositivo: ")
+
+        # print(self.mostrar_dispositivo(id_dispositivo).strip().split("|")[3].get_triggers())
+        comando = input("Comando: ")
+        argumentos = input("Argumentos (k=v separados por espaco) ou ENTER: ")
 
 
     def rotinas(self, tipo_rotina:str):
@@ -51,3 +121,12 @@ class Hub:
                 if isinstance(dispositivo, Persiana):
                     dispositivo.abrir()
 
+
+
+if __name__ == "__main__":
+    # porta = Porta("porta_sala", "porta da sala")
+    hub = Hub()
+    # dispositivos = [{"id": porta.id, "nome": porta.nome, "tipo": porta.tipo, "estado": porta.state.name}]
+    hub.adicionar_dispositivo()
+    hub.executar_comando()
+    hub.remover_dispositivo()
