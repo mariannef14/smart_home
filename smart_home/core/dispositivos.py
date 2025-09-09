68457488611008105a833e1d8779315bb8d55d25
Marianne Félix <mariannefelixs11@gmail.com>
2025-09-08 23:51:28 -0300

    refactor: add parametro tipo dispositivo no construtor Dispositivo

--- smart_home/core/dispositivos.py
+++ smart_home/core/dispositivos.py
@@ -2,58 +2,6 @@ from enum import Enum
 from abc import ABC
 
 
-class Dispositivo(ABC):
-
-    def __init__(self, id:str, nome:str):
-        self.id = id
-        self.nome = nome
-
-
-class StatesPorta(Enum):
-
-    TRANCADA = 0
-    DESTRANCADA = 1
-    ABERTA = 2
-
-
-    def __str__(self) -> str:
-        return self.name
-
-
-class CorEnum(Enum):
-
-    NEUTRA = 0,
-    FRIA = 1,
-    QUENTE = 2
-
-    
-    def __str__(self) -> str:
-        return self.name
-
-
-class StatusCamera(Enum):
-
-    ON = 0 
-    GRAVANDO = 1
-    STOP = 2
-    OFF = 3
-
-
-    def __str__(self) -> str:
-        return self.name
-
-
-class StatusIrrigador(Enum):
-
-    LIGADO = 0,
-    IRRIGANDO = 1,
-    DESLIGADO = 2
-
-
-    def __str__(self) -> str:
-        return self.name
-
-
 class TiposDispostivos(Enum):
 
     CAMERA = 0,
@@ -73,3 +21,64 @@ class TiposDispostivos(Enum):
         dispositivos = [dispositivo.name for dispositivo in list(TiposDispostivos)]
 
         return ", ".join(dispositivos)
+
+
+
+class Dispositivo(ABC):
+
+    def __init__(self, id:str, nome:str, tipo_dispositivo: TiposDispostivos):
+        self.id = id
+        self.nome = nome
+        self.tipo = tipo_dispositivo
+
+    def __str__(self):
+        return f"{self.id} | {self.tipo}"
+
+
+
+class StatesPorta(Enum):
+
+    TRANCADA = 0
+    DESTRANCADA = 1
+    ABERTA = 2
+
+
+    def __str__(self) -> str:
+        return self.name
+
+
+
+class CorEnum(Enum):
+
+    NEUTRA = 0,
+    FRIA = 1,
+    QUENTE = 2
+
+    
+    def __str__(self) -> str:
+        return self.name
+
+
+
+class StatusCamera(Enum):
+
+    ON = 0 
+    GRAVANDO = 1
+    STOP = 2
+    OFF = 3
+
+
+    def __str__(self) -> str:
+        return self.name
+
+
+
+class StatusIrrigador(Enum):
+
+    LIGADO = 0,
+    IRRIGANDO = 1,
+    DESLIGADO = 2
+
+
+    def __str__(self) -> str:
+        return self.name
