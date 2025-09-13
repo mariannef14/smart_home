d7a12bdac539cfd27a16dc34a987897ba7767843
Marianne Félix <mariannefelixs11@gmail.com>
2025-09-13 18:59:48 -0300

    feat: add property potencia and consumo em tomada

--- smart_home/dispositivos/tomada.py
+++ smart_home/dispositivos/tomada.py
@@ -28,16 +28,24 @@ transitions = [
 @dataclass
 class Tomada(Dispositivo):
 
-    id:str
-    nome:str
-    potencia_w:int = PotenciaValidator()
-    tipo:TiposDispostivos = TiposDispostivos.TOMADA
+    _potencia_w:int = PotenciaValidator()
+    tipo:TiposDispostivos = field(init = False, default = TiposDispostivos.TOMADA)
 
 
     def __post_init__(self):
         self.machine = Machine(model = self, states = ["On", "Off"], transitions = transitions, initial = "Off")
         self.hora_tomada_ligou = None
-        self.consumo_wh = 0
+        self._consumo_wh = 0
+    
+    
+    @property
+    def potencia_w(self):
+        return self._potencia_w
+
+
+    @property
+    def consumo_wh(self):
+        return self._consumo_wh
     
 
     def on_enter_On(self):
@@ -47,18 +55,20 @@ class Tomada(Dispositivo):
     def on_enter_Off(self):
         hora_tomada_desligou = datetime.now()
         horas_ligada = ((hora_tomada_desligou - self.hora_tomada_ligou).total_seconds() / 3600)
-        self.consumo_wh = abs(self.potencia_w * horas_ligada)
+        self._consumo_wh = abs(self._potencia_w * horas_ligada)
 
 
 
 if __name__ == '__main__':
     
     tomada = Tomada("tomada_tv", "tomada da tv", 220)
-    print(tomada.potencia_w)
+    print("Id:", tomada.id)
+    print("Tipo do dispositivo:", tomada.tipo)
+    print("Potência: ", tomada.potencia_w)
     tomada.ligar()
-    print(tomada.state)
+    print("Status:", tomada.state)
     print(tomada.hora_tomada_ligou)
     sleep(4)
     tomada.desligar()
-    print(tomada.consumo_wh)
-    print(tomada.state)
+    print("Consumo total:", tomada.consumo_wh)
+    print("Status:", tomada.state)
