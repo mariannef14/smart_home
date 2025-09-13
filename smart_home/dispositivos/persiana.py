25f28a0351449ad3bca88bee5b6537a102281a37
Marianne Félix <mariannefelixs11@gmail.com>
2025-09-13 19:00:12 -0300

    feat: add property percentual abertura em persiana

--- smart_home/dispositivos/persiana.py
+++ smart_home/dispositivos/persiana.py
@@ -35,22 +35,25 @@ transitions = [
 @dataclass
 class Persiana(Dispositivo):
 
-    id:str
-    nome:str
-    tipo = TiposDispostivos.PERSIANA
-    porcentagem_aberta:int = field(default = PorcentagemValidator())
+    tipo:TiposDispostivos = field(init = False, default = TiposDispostivos.PERSIANA)
+    _percentual_abertura:int = field(init = False, default = PorcentagemValidator())
 
 
     def __post_init__(self):
         self.machine = Machine(model = self, states = ["Open", "Closed"], transitions = transitions, initial = "Closed", auto_transitions = False)
     
 
+    @property
+    def percentual_abertura(self):
+        return self._percentual_abertura
+
+
     def on_enter_Closed(self):
-        self.porcentagem_aberta = 0
+        self._percentual_abertura = 0
     
 
     def porcentagem_abertura(self, value):
-        self.porcentagem_aberta = value
+        self._percentual_abertura = value
 
 
     def __str__(self):
@@ -61,11 +64,13 @@ class Persiana(Dispositivo):
 if __name__ == "__main__":
 
     persiana = Persiana("persiana_quarto", "persiana do quarto")
+    print("Id:", persiana.id)
+    print("Tipo do dispositivo:", persiana.tipo)
     persiana.abrir()
-    print(persiana.state)
-    persiana.definir_porcentagem_abertura(value = 10)
-    print(persiana.state)
-    print(f"Persiana com {persiana.porcentagem_aberta}% aberta")
+    print("Status:", persiana.state)
+    persiana.definir_porcentagem_abertura(value = 15)
+    print("Status:", persiana.state)
+    print(f"Persiana com {persiana.percentual_abertura}% aberta")
     persiana.fechar()
-    print(persiana.state)
-    print(persiana.porcentagem_aberta)
+    print("Status:", persiana.state)
+    print(f"Persiana com {persiana.percentual_abertura}% aberta")
