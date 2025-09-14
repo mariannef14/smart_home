e71216d409192fd0ad2b3c13cf02444d751926f2
Marianne Félix <mariannefelixs11@gmail.com>
2025-09-13 23:29:48 -0300

    feat: add alterar atributo

--- smart_home/core/hub2.py
+++ smart_home/core/hub2.py
@@ -1,14 +1,16 @@
 from transitions import Machine
-from dataclasses import dataclass, field, asdict
+from dataclasses import dataclass, field
 from typing import List
 
-from smart_home.core.dispositivos import TiposDispostivos, CorEnum
+from smart_home.core.dispositivos import TiposDispostivos, CorEnum, StatesPorta
 from smart_home.dispositivos.porta import Porta
 from smart_home.dispositivos.luz import Luz
 from smart_home.dispositivos.tomada import Tomada
 from smart_home.dispositivos.irrigador import Irrigador
 from smart_home.dispositivos.persiana import Persiana
 from smart_home.dispositivos.camera import Camera
+
+from smart_home.core.logger import Logger
 
 
 @dataclass
@@ -18,6 +20,8 @@ class Hub:
 
     
     def adicionar_dispositivo(self):
+        dispositivo = None
+
         print("Tipos suportados:", TiposDispostivos.all_dispositives())
         tipo_dispositivo = input("Tipo do dispositivo: ").upper().strip()
         id_dispositivo = input("Id (sem espacos): ").lower().strip()
@@ -25,7 +29,8 @@ class Hub:
 
 
         if tipo_dispositivo == TiposDispostivos.PORTA.name:
-            self.dispositivos.append(Porta(id_dispositivo, nome_dispositivo))
+            dispositivo = Porta(id_dispositivo, nome_dispositivo)
+            self.dispositivos.append(dispositivo)
         
 
         elif tipo_dispositivo == TiposDispostivos.LUZ.name:
@@ -35,25 +40,38 @@ class Hub:
             brilho_luz = 50 if brilho_luz == "" else brilho_luz
             cor_luz = "NEUTRA" if cor_luz == "" else cor_luz
 
-            self.dispositivos.append(Luz(id_dispositivo, nome_dispositivo, int(brilho_luz), cor_luz))
+            dispositivo = Luz(id_dispositivo, nome_dispositivo, int(brilho_luz), cor_luz)
+
+            self.dispositivos.append(dispositivo)
         
 
         elif tipo_dispositivo == TiposDispostivos.TOMADA.name:
             potencia_w = int(input("Potência da tomada: "))
-            self.dispositivos.append(Tomada(id_dispositivo, nome_dispositivo, potencia_w))
+            dispositivo = Tomada(id_dispositivo, nome_dispositivo, potencia_w)
+            self.dispositivos.append(dispositivo)
         
 
         elif tipo_dispositivo == TiposDispostivos.IRRIGADOR.name:
-            self.dispositivos.append(Irrigador(id_dispositivo, nome_dispositivo))
+            dispositivo = Irrigador(id_dispositivo, nome_dispositivo)
+            self.dispositivos.append(dispositivo)
         
 
         elif tipo_dispositivo == TiposDispostivos.PERSIANA.name:
             porcentagem = int(input("Deseja abrir quantos porcentos da persiana? (0-100): "))
-            self.dispositivos.append(Persiana(id_dispositivo, nome_dispositivo, porcentagem))
+            dispositivo = Persiana(id_dispositivo, nome_dispositivo, porcentagem)
+            self.dispositivos.append(dispositivo)
         
         elif tipo_dispositivo == TiposDispostivos.CAMERA.name:
-            self.dispositivos.append(Camera(id_dispositivo, nome_dispositivo))
-    
+            dispositivo = Camera(id_dispositivo, nome_dispositivo)
+            self.dispositivos.append(dispositivo)
+        
+        else:
+            #TODO: LANÇAR EXCEÇÃO PERSONALIZADA PARA TIPO DE DISPOSITIVO INVÁLIDO(ConfigInvalida)
+            ...
+        
+        logger = Logger()
+        logger.evento(dispositivo, "adicionar")
+
 
     def mostrar_dispositivo(self):
 
@@ -125,31 +143,113 @@ class Hub:
             print(dispositivo.percentual_abertura)
 
 
-    def rotinas(self, tipo_rotina:str):
+    def alterar_atributo(self):
+        print("Atributos que podem ser modificados [LUZ]: brilho e cor, [PERSIANA]: porcentagem de abertura(abertura)")
+        dispositivo = self.mostrar_dispositivo()
 
-        for dispositivo in self.FMS:
+        if dispositivo.tipo == TiposDispostivos.LUZ or dispositivo.tipo == TiposDispostivos.PERSIANA: 
 
-            if tipo_rotina.lower().strip() == "modo_noite":
+
+            argumentos = input("Argumentos (k=v separados por espaco) ou ENTER: ")
+
+
+            if argumentos != "":
+                    argumentos_dict = dict(argumento.split("=") for argumento in argumentos.split())
+
+                    #TODO: TIRAR ESSE PRINT
+                    print(argumentos_dict)
+                    self.mudar_atributos(argumentos_dict, dispositivo)
+
+        else:
+            raise ValueError("Esse dispositivo não possui atributos que podem ser alterados")
+
+
+    def executar_rotina(self):
+
+        rotina = input("Digite o tipo de rotina que deseja executar (dormir ou acordar): ").lower().strip()
+
+        for dispositivo in self.dispositivos:
+
+            if rotina == "dormir":
 
                 if isinstance(dispositivo, Porta):
-                    dispositivo.trancar()
+                    
+                    state_porta = dispositivo.state
+
+                    while state_porta != StatesPorta.TRANCADA:
+
+                        trigger = dispositivo.machine.get_triggers(dispositivo.state)
+                        #TODO: TIRAR ESSE PRINT
+                        print(trigger)
+                        dispositivo.trigger(trigger[0])
+                        state_porta = dispositivo.state
+                        #TODO: TIRAR ESSE PRINT
+                        print("Status: ", dispositivo.state)
+                
+                    #TODO: TIRAR ESSE PRINT
+                    print("Status final da porta:", dispositivo.state)
                 
                 if isinstance(dispositivo, Luz):
-                    dispositivo.desligar()
+
+                    if dispositivo.state == "On":
+                        dispositivo.desligar()
+
+                    #TODO: TIRAR ESSE PRINT
+                    print("Status final da luz:", dispositivo.state)
                 
                 if isinstance(dispositivo, Tomada):
-                    dispositivo.desligar()
-                
-                if isinstance(dispositivo, Persiana):
-                    dispositivo.fechar()
 
-            elif tipo_rotina.lower().strip() == "acordar":
+                    if dispositivo.state == "On":
+                        dispositivo.desligar()
+                    
+                    #TODO: TIRAR ESSE PRINT
+                    print("Status final da tomada:", dispositivo.state)
+
+                if isinstance(dispositivo, Persiana):
+
+                    if dispositivo.state == "Open":
+                        dispositivo.fechar()
+
+                    #TODO: TIRAR ESSE PRINT
+                    print("Status final da persiana:", dispositivo.state)
+
+
+            elif rotina == "acordar":
                 
                 if isinstance(dispositivo, Luz):
                     dispositivo.ligar()
+                    dispositivo.definir_brilho(value = 50)
+                    #TODO: TIRAR ESSE PRINT
+                    print("Status inicial da luz:", dispositivo.state)
+
                 
                 if isinstance(dispositivo, Persiana):
                     dispositivo.abrir()
+                    dispositivo.definir_porcentagem_abertura(value = 50)
+                    #TODO: TIRAR ESSE PRINT
+                    print("Status inicial da persiana:", dispositivo.state)
+
+                
+                if isinstance(dispositivo, Irrigador):
+                    dispositivo.ligar()
+                    #TODO: TIRAR ESSE PRINT
+                    print("Status inicial do irrigador:", dispositivo.state)
+                    dispositivo.irrigar()
+
+        else:
+            #TODO: LANÇAR EXCEÇÃO PERSONALIZADA PARA ROTINA NÃO EXISTENTE(ConfigInvalida)
+            ...
+
+
+    def salvar_configuracao(self):
+                # dispositivo = {
+        #     "id": id_dispostivo,
+        #     "tipo": tipo_dispositivo,
+        #     "nome": nome_dispositivo,
+        #     "estado": 
+        # }
+        ...
+
 
 
 
@@ -157,8 +257,14 @@ if __name__ == "__main__":
     hub = Hub()
     hub.adicionar_dispositivo()
     # hub.adicionar_dispositivo()
-    # hub.listar_dispositivos()
-    # hub.mostrar_dispositivo()
+    # hub.adicionar_dispositivo()
+    # hub.adicionar_dispositivo()
+    # hub.adicionar_dispositivo()
+    hub.listar_dispositivos()
+    hub.mostrar_dispositivo()
+    hub.alterar_atributo()
     hub.executar_comando()
+    # hub.executar_comando()
     # hub.remover_dispositivo()
-    # hub.remover_dispositivo()
+    hub.executar_rotina()
+    # hub.executar_rotina()
