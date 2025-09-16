b7a25860740e17be480c6b8cdf2f0cc9f6b13723
Marianne Félix <mariannefelixs11@gmail.com>
2025-09-15 22:53:15 -0300

    feat: add novos eventos

--- smart_home/core/eventos.py
+++ smart_home/core/eventos.py
@@ -3,7 +3,7 @@ from enum import Enum
 
 class Eventos(Enum):
     ADICIONAR_DISPOSITIVO = "adicionar"
-    ADICIONAR_DISPOSITIVOS = "adicionar dispositivos"
-    ALTERAR_ATRIBUTO_DISPOSITIVO = "alterar atributo"
     EXECUTAR_COMANDO_DISPOSITIVO  ="executar comando"
     REMOVER_DISPOSITIVO = "remover"
+    ADICIONAR_DISPOSITIVOS_JSON = "json"
+    EXECUTAR_ROTINA = "rotina"
