448e155cc1b035c74ad249341455b4a651068e69
Marianne Félix <mariannefelixs11@gmail.com>
2025-09-16 00:29:02 -0300

    feat: add executar rotina

--- smart_home/core/observers.py
+++ smart_home/core/observers.py
@@ -6,6 +6,12 @@ from datetime import datetime
 
 from smart_home.core.eventos import Eventos
 from smart_home.core.dispositivos import TiposDispostivos
+from smart_home.dispositivos.porta import Porta
+from smart_home.dispositivos.luz import Luz
+from smart_home.dispositivos.irrigador import Irrigador
+from smart_home.dispositivos.persiana import Persiana
+from smart_home.core.dispositivos import StatesPorta
+from smart_home.core.erros import ConfigInvalida
 
 
 class Sujeito:
@@ -24,7 +30,7 @@ class Sujeito:
         self.observadores.remove(observador)
 
     
-    def notificar(self, tipo_evento, dispositivo, trigger = ""):
+    def notificar(self, tipo_evento, dispositivo = "", trigger = ""):
 
         for observador in self.observadores:
             observador.atualizar(tipo_evento, dispositivo, trigger)
@@ -41,219 +47,88 @@ class Observador(ABC):
 
 class CliObserver(Observador):
     
-    #TODO: TRANSFORMAR DISPOSITIVO PARA RECEBER VÁRIOS E NO INÍCIO DO HUB CHAMAR O ADICIONAR PARA
-    #ADICIONAR A LISTA OU CRIAR UM NOVO EVENTO SÓ PARA ADICIONAR OS DO JSON E ADICIONAR NA LISTA
-    def atualizar(self, tipo_evento, dispositivo, trigger):
 
-        if tipo_evento == Eventos.ADICIONAR_DISPOSITIVO.value:
-            # self._adicionar_dispositivo_json(dispositivo)
-            dispositivo_dict = self._parse_object_to_dict(dispositivo)
-            self._adicionar_dispositivo_dict(dispositivo_dict)
+    def atualizar(self, tipo_evento, dispositivo, trigger):
         
-        if tipo_evento == Eventos.ADICIONAR_DISPOSITIVOS.value:
+        if tipo_evento == Eventos.ADICIONAR_DISPOSITIVOS_JSON.value:
             self._adicionar_dispositivos_json(dispositivo)
-        
-        elif tipo_evento == Eventos.REMOVER_DISPOSITIVO.value:
-            self._remover_dispositivo_json(dispositivo)
         
         elif tipo_evento == Eventos.EXECUTAR_COMANDO_DISPOSITIVO.value:
             self._adicionar_transicao_csv(dispositivo, trigger)
-            #TODO: MODIFICAR NO JSON
+        
+        elif tipo_evento == Eventos.EXECUTAR_ROTINA.value:
+            self._executar_rotina(dispositivo, trigger)
 
-
-    def _adicionar_dispositivo_dict(self, dispositivo):
-        ...
 
     def _parse_object_to_dict(self, dispositivo):
-        
+                          
          if dispositivo.tipo == TiposDispostivos.LUZ or dispositivo.tipo == TiposDispostivos.PERSIANA or dispositivo.tipo == TiposDispostivos.TOMADA:
                 estado = dispositivo.state
-                print("entrei")
+         
          else:
             estado = dispositivo.state.name
 
 
-            if dispositivo.tipo == TiposDispostivos.LUZ:
-            
-                dispositivo_dict = {
-                    "id": dispositivo.id,
-                    "tipo": dispositivo.tipo,
-                    "nome": dispositivo.nome,
-                    "estado": estado,
-                    "atributos": {
-                        "brilho": dispositivo.brilho,
-                        "cor": dispositivo.cor
-                    }
-                }
+         if dispositivo.tipo == TiposDispostivos.LUZ:
         
-            elif dispositivo.tipo == TiposDispostivos.PERSIANA:
-                dispositivo_dict = {
-                    "id": dispositivo.id,
-                    "tipo": dispositivo.tipo,
-                    "nome": dispositivo.nome,
-                    "estado": estado,
-                    "atributos": {"percentual_abertura": dispositivo.percentual_abertura}
+            dispositivo_dict = {
+                "id": dispositivo.id,
+                "tipo": dispositivo.tipo,
+                "nome": dispositivo.nome,
+                "estado": estado,
+                "atributos": {
+                    "brilho": dispositivo.brilho,
+                    "cor": dispositivo.cor
                 }
-            
-            elif dispositivo.tipo == TiposDispostivos.TOMADA:
-                dispositivo_dict = {
-                    "id": dispositivo.id,
-                    "tipo": dispositivo.tipo,
-                    "nome": dispositivo.nome,
-                    "estado": estado,
-                    "atributos": {"potencia": dispositivo.potencia_w}
-                }
+            }
         
-            else:
-                dispositivo_dict = {
-                    "id": dispositivo.id,
-                    "tipo": dispositivo.tipo,
-                    "nome": dispositivo.nome,
-                    "estado": estado,
-                    "atributos": {}
-                }
-
-            return dispositivo_dict
-
-
-    def _adicionar_dispositivo_json(self, dispositivo):
-
-        #TODO: pegar objeto e converter para dicionario
-        #TODO: pegar esse objeto dicionario e adicionar em uma lista 
-        #TODO: pegar essa lista e adicionar no json
-        with open(file = "data/configuracao.json", mode = "r", encoding = "utf-8") as file:
-            dispositivos = json.load(file)
-
-        with open(file = "data/configuracao.json", mode = "w", encoding = "utf-8") as file:
-
-             if dispositivo.tipo == TiposDispostivos.LUZ or dispositivo.tipo == TiposDispostivos.PERSIANA or dispositivo.tipo == TiposDispostivos.TOMADA:
-                estado = dispositivo.state
-                print("entrei")
-             
-             else:
-                estado = dispositivo.state.name
+         elif dispositivo.tipo == TiposDispostivos.PERSIANA:
+            dispositivo_dict = {
+                "id": dispositivo.id,
+                "tipo": dispositivo.tipo,
+                "nome": dispositivo.nome,
+                "estado": estado,
+                "atributos": {"percentual_abertura": dispositivo.percentual_abertura}
+            }
+        
+         elif dispositivo.tipo == TiposDispostivos.TOMADA:
+            dispositivo_dict = {
+                "id": dispositivo.id,
+                "tipo": dispositivo.tipo,
+                "nome": dispositivo.nome,
+                "estado": estado,
+                "atributos": {"potencia": dispositivo.potencia_w}
+            }
     
+         else:
+            dispositivo_dict = {
+                "id": dispositivo.id,
+                "tipo": dispositivo.tipo,
+                "nome": dispositivo.nome,
+                "estado": estado,
+                "atributos": {}
+            }
         
-             if dispositivo.tipo == TiposDispostivos.LUZ:
-                
-                dispositivo_dict = {
-                    "id": dispositivo.id,
-                    "tipo": dispositivo.tipo,
-                    "nome": dispositivo.nome,
-                    "estado": estado,
-                    "atributos": {
-                        "brilho": dispositivo.brilho,
-                        "cor": dispositivo.cor
-                    }
-                }
-            
-             elif dispositivo.tipo == TiposDispostivos.PERSIANA:
-                dispositivo_dict = {
-                    "id": dispositivo.id,
-                    "tipo": dispositivo.tipo,
-                    "nome": dispositivo.nome,
-                    "estado": estado,
-                    "atributos": {"percentual_abertura": dispositivo.percentual_abertura}
-                }
-            
-             elif dispositivo.tipo == TiposDispostivos.TOMADA:
-                dispositivo_dict = {
-                    "id": dispositivo.id,
-                    "tipo": dispositivo.tipo,
-                    "nome": dispositivo.nome,
-                    "estado": estado,
-                    "atributos": {"potencia": dispositivo.potencia_w}
-                }
-            
-             else:
-                dispositivo_dict = {
-                    "id": dispositivo.id,
-                    "tipo": dispositivo.tipo,
-                    "nome": dispositivo.nome,
-                    "estado": estado,
-                    "atributos": {}
-                }
 
-             dispositivos["dispositivos"].append(dispositivo_dict)
-             json.dump(dispositivos, file, indent = 2)
+         return dispositivo_dict
 
 
     def _adicionar_dispositivos_json(self, dispositivos):
 
-        with open(file = "data/configuracao.json", mode = "r", encoding = "utf-8") as file:
+        with open(file = "data/configuracao.json", mode = "r") as file:
             dispositivos_json = json.load(file)
 
-        with open(file = "data/configuracao.json", mode = "w", encoding = "utf-8") as file:
-             
-            #TODO: adicionar chave com o nome dispositivos tipo o da tarefa
-             lista_dispositivos = dispositivos_json.get("dispositivos")
+        with open(file = "data/configuracao.json", mode = "w") as file:
 
-             ids = [dispositivo.get("id") for dispositivo in lista_dispositivos]
+            dispositivos_json.get("dispositivos").clear()
+
+
+            for dispositivo in dispositivos:
+                dispositivo_dict = self._parse_object_to_dict(dispositivo)
+
+                dispositivos_json["dispositivos"].append(dispositivo_dict)
             
-             for dispositivo in dispositivos:
-
-                if dispositivo.id not in ids:
-                    print(dispositivo.id, "não está")
-
-                    if dispositivo.tipo == TiposDispostivos.LUZ or dispositivo.tipo == TiposDispostivos.PERSIANA or dispositivo.tipo == TiposDispostivos.TOMADA:
-                        estado = dispositivo.state
-                    
-                    else:
-                        estado = dispositivo.state.name
-            
-                
-                    if dispositivo.tipo == TiposDispostivos.LUZ:
-                        
-                        dispositivo_dict = {
-                            "id": dispositivo.id,
-                            "tipo": dispositivo.tipo,
-                            "nome": dispositivo.nome,
-                            "estado": estado,
-                            "atributos": {
-                                "brilho": dispositivo.brilho,
-                                "cor": dispositivo.cor
-                            }
-                        }
-                    
-                    elif dispositivo.tipo == TiposDispostivos.PERSIANA:
-                        dispositivo_dict = {
-                            "id": dispositivo.id,
-                            "tipo": dispositivo.tipo,
-                            "nome": dispositivo.nome,
-                            "estado": estado,
-                            "atributos": {"percentual_abertura": dispositivo.percentual_abertura}
-                        }
-                    
-                    else:
-                        dispositivo_dict = {
-                            "id": dispositivo.id,
-                            "tipo": dispositivo.tipo,
-                            "nome": dispositivo.nome,
-                            "estado": estado,
-                            "atributos": {}
-                        }
-
-                    dispositivos_json["dispositivos"].append(dispositivo_dict)
-                
-                json.dump(dispositivos_json, file, indent = 2)
-
-
-    def _remover_dispositivo_json(self, dispositivo):
-
-        with open(file = "data/configuracao.json", mode = "r") as file:
-            dispositivos = json.load(file)
-
-            lista_dispositivos = dispositivos.get("dispositivos")
-
-            for i, d in enumerate(lista_dispositivos):
-                if d.get("id") == dispositivo.id:
-                    lista_dispositivos.pop(i)
-                    break
-            
-        
-        with open(file = "data/configuracao.json", mode = "w") as file: 
-
-            json.dump(dispositivos, file, indent = 2)
+            json.dump(dispositivos_json, file, indent = 2) 
 
 
     def _adicionar_transicao_csv(self, dispositivo, trigger):
@@ -262,8 +137,8 @@ class CliObserver(Observador):
 
         for source, transicoes in evento.transitions.items():
             for t in transicoes:
-               source = t.source
-               dest = t.dest
+                source = t.source
+                dest = t.dest
 
 
         with open(file = "data/events.csv", mode = "a", newline = "", encoding = "utf-8") as file:
@@ -272,3 +147,57 @@ class CliObserver(Observador):
             
             writer.writerow([datetime.now().strftime("%Y-%m-%dT%H:%M:%S"), dispositivo.id, dispositivo.tipo, trigger, source.lower(), dest.lower()])
 
+
+
+    def _executar_rotina(self, dispositivos, trigger):
+
+        with open(file = "data/configuracao.json", mode = "r") as file:
+            dispositivos_json = json.load(file)
+
+
+        if trigger not in dispositivos_json.get("rotinas"):
+            raise ConfigInvalida("Essa rotina não existe")
+
+        dispositivos_rotina = dispositivos_json.get("rotinas").get(trigger)
+        ids_dispositivos_rotina = [dispositivo.get("id") for dispositivo in dispositivos_rotina]
+
+        
+        for dispositivo in dispositivos:
+
+            if trigger == "dormir":
+            
+                if dispositivo.id in ids_dispositivos_rotina:
+
+                    if isinstance(dispositivo, Porta):
+                    
+                        state_porta = dispositivo.state
+
+                        while state_porta != StatesPorta.TRANCADA:
+
+                            trigger = dispositivo.machine.get_triggers(dispositivo.state)
+                            dispositivo.trigger(trigger[0])
+                            state_porta = dispositivo.state
+                
+                    if isinstance(dispositivo, Luz):
+
+                        if dispositivo.state == "On":
+                            dispositivo.desligar()
+                    
+                    if isinstance(dispositivo, Persiana):
+
+                        if dispositivo.state == "Open":
+                            dispositivo.fechar()
+            
+            elif trigger == "acordar":
+                
+                if isinstance(dispositivo, Luz):
+                    dispositivo.ligar()
+                    dispositivo.definir_brilho(value = 50)
+   
+                if isinstance(dispositivo, Persiana):
+                    dispositivo.abrir()
+                    dispositivo.definir_porcentagem_abertura(value = 30)
+                
+                if isinstance(dispositivo, Irrigador):
+                    dispositivo.ligar()
+                    dispositivo.irrigar()
